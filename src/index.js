9aec2c87549faa7b53f503a2fe838acffe1881d3
Norbory <mandros.angelof@pucp.edu.pe>
2023-12-10 15:20:53 -0500

    Added cors and change Port to 3001

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 import express from 'express';
-import projectsRoute from './routes/projects.routes.js'
-import indexRoute from './routes/index.routes.js'
+import cors from 'cors';
+import projectsRoute from './routes/projects.routes.js';
+import indexRoute from './routes/index.routes.js';
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.use(indexRoute);
-app.use("/api",projectsRoute);
+app.use("/api", projectsRoute);
 
-app.listen(3000)
-console.log("Server running")
+app.use((req, res, next) => {
+    res.status(404).json({
+        message: "Página no encontrada"
+    });
+});
+
+app.listen(3001);
+console.log("Servidor en ejecución");
