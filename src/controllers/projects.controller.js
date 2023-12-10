68da7ed8be50b02f7ddfd8f3ffe57a605be38b9b
Norbory <mandros.angelof@pucp.edu.pe>
2023-12-10 16:45:14 -0500

    Changed CRUD methods

--- src/controllers/projects.controller.js
+++ src/controllers/projects.controller.js
@@ -12,69 +12,69 @@ export const getProjects = async (req,res) => {
 };
 
 export const getProjectById = async (req,res) => {
-    try{
-        const [rows] = await conn.query('SELECT * FROM projects WHERE id=?', [req.params.id]);
-        if (rows.length <= 0) return res.status(404).json({
-            message: "No existe el proyecto que estas solicitando"
-        })
-        res.json(rows[0])
-    }catch(error){
-        res.status(500).json({
-            message:"Algo salio mal"
-        })
-    }
-};
-
+    try {
+        const { id } = req.params;
+        const [rows] = await conn.query("SELECT * FROM projects WHERE id = ?", [
+          id,
+        ]);
+    
+        if (rows.length <= 0) {
+          return res.status(404).json({ message: "Project not found" });
+        }
+    
+        res.json(rows[0]);
+      } catch (error) {
+        return res.status(500).json({ message: "Something goes wrong" });
+      }
+    };
+    
 export const deleteProjects =  async (req,res) => {
-    try{
-        const [rows] = await conn.query('SELECT * FROM projects WHERE id=?', [req.params.id]);
-        if (rows.length <= 0) return res.status(404).json({
-            message: "No existe el proyecto que estas solicitando"
-        })
-        res.json(rows[0])
-
-        const [result] = await conn.query('DELETE FROM projects WHERE id=?', [req.params.id]);
-        if (result.affectedRows <= 0) return res.status(404).json({
-            message:"Proyecto no encontrado"
-        })
-        res.sendStatus(204)
-    }catch(error){
-        res.status(500).json({
-            message:"Algo salio mal"
-        })
-    }
-};
+    try {
+        const { id } = req.params;
+        const [rows] = await conn.query("DELETE FROM projects WHERE id = ?", [id]);
+    
+        if (rows.affectedRows <= 0) {
+          return res.status(404).json({ message: "Project not found" });
+        }
+    
+        res.sendStatus(204);
+      } catch (error) {
+        return res.status(500).json({ message: "Something goes wrong" });
+      }
+    };
 
 export const createProjects = async (req,res) => {
-    const {name} = req.body;
-    try{
-        const [rows] = await conn.query('INSERT INTO projects (name) VALUES (?)', name);
-        res.send({
-            id: rows.insertId,
-            name,
-        });
-    }catch(error){
-        res.status(500).json({
-            message:"Algo salio mal"
-        })
-    }
-};
+    try {
+        const { name } = req.body;
+        const [rows] = await conn.query(
+          "INSERT INTO projects (name) VALUES (?)",
+          name
+        );
+        res.status(201).json({ id: rows.insertId, name });
+      } catch (error) {
+        return res.status(500).json({ message: "Something goes wrong" });
+      }
+    };
 
 export const updateProjects = async (req,res) => {
-    const {id} = req.params;
-    const {name} = req.body;
-    try{
-        const [result] = await conn.query('UPDATE projects SET name = IFNULL(?,name) WHERE id = ?', [name, id]);
-        if (result.affectedRows <=0) return res.status(404).json({
-            message:"Proyecto no valido"
-        })
-
-        const [rows] = await conn.query('SELECT * FROM projects WHERE id = ?', [id])
+    try {
+        const { id } = req.params;
+        const { name } = req.body;
+    
+        const [result] = await conn.query(
+          "UPDATE projects SET name = IFNULL(?, name) WHERE id = ?",
+          [name, id]
+        );
+    
+        if (result.affectedRows === 0)
+          return res.status(404).json({ message: "Project not found" });
+    
+        const [rows] = await conn.query("SELECT * FROM projects WHERE id = ?", [
+          id,
+        ]);
+    
         res.json(rows[0]);
-    }catch(error){
-        res.status(500).json({
-            message:"Algo salio mal"
-        })
-    }
-};
-
+      } catch (error) {
+        return res.status(500).json({ message: "Something goes wrong" });
+      }
+    };
