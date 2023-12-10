f4af9c74a8f31313bb3662ca54eb2625442f5251
Norbory <mandros.angelof@pucp.edu.pe>
2023-12-09 19:10:04 -0500

    First one

---
+++ db.js
@@ -0,0 +1,11 @@
+import {createPool} from 'mysql2';
+
+export const conn = createPool({
+    host: 'localhost',
+    user: 'root',
+    password: 'admin',
+    port: '3306',
+    database: 'geodigital'
+})
+
+// conn.query('SELECT * FROM projects', (err, result) => )
---
+++ db/database.sql
@@ -0,0 +1,23 @@
+    CREATE DATABASE IF NOT EXISTS geodigital;
+
+    USE geodigital;
+
+    CREATE TABLE projects (
+        id INT NOT NULL AUTO_INCREMENT,
+        name VARCHAR(100) NOT NULL,
+        primary key (id)
+    );
+
+    DESCRIBE projects;
+
+     CREATE TABLE storage (
+        id_project INT NOT NULL AUTO_INCREMENT,
+        name VARCHAR(100) NOT NULL,
+        description VARCHAR(500) DEFAULT NULL,
+        location VARCHAR(500) DEFAULT NULL,
+        stock INT NOT NULL,
+        id INT NOT NULL AUTO_INCREMENT,
+        primary key (id)
+    );
+
+    DESCRIBE storage;
